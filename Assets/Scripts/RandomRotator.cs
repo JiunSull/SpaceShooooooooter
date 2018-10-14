360b56d30bc733dd76c4edc8837855faa6827914
Ichiro <1027952@etusd.org>
2018-10-14 10:05:54 -0700

    intial commit

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public Rigidbody r;
+	public float tumble;
+
+	void Start () 
+	{
+
+		r.angularVelocity = Random.insideUnitSphere * tumble;
+		
+	}
+	
+
+}
